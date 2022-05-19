96dbc4add4208a9e4d4788edaa1e940cb60dc346
Brymer <meneses.brymer@gmail.com>
2022-05-19 19:03:09 +0800

    add example default data

--- src/main/MP/StudentDBDemo.java
+++ src/main/MP/StudentDBDemo.java
@@ -1,11 +1,11 @@
 package MP;
 
-
 public class StudentDBDemo {
     public static void main(String[] args) {
 
         StudentDB sdb = new StudentDB();
-        
+        sdb.addData(new StudentData("Leonhard Euler", 045612, 17150234, "St. Petersburg, Berlin, Russia"));
+
         App app = new App();
         app.initialize(args, sdb);
     }
