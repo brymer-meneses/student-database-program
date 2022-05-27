191251495aab7d6b90c989f9ab62ac11275fde36
Brymer <meneses.brymer@gmail.com>
2022-05-27 14:50:56 +0800

    last fix was an oopsie lol

--- src/main/MP/StudentDB.java
+++ src/main/MP/StudentDB.java
@@ -255,14 +255,14 @@ public class StudentDB implements Serializable, DBInterface {
     public void searchData(String toSearch) {
         searchEntries.getChildren().clear();
 
-        boolean isSearchResultZero = false;
+        boolean isSearchResultZero = true;
         for (int i=0; i<database.length; i++) {
             StudentData element = database.get(i);
 
             if (Utils.keywordInEntries(database.get(i), toSearch)) {
                 DatabaseEntry entry = new DatabaseEntry("search", element);
                 searchEntries.getChildren().add(entry);
-                isSearchResultZero = true;
+                isSearchResultZero = false;
             }
 
         }
